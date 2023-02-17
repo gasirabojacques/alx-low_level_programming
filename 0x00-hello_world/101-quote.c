bd59fb96b75dd8d1ed69ce892354624485aadd28
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-02-16 17:09:08 -0800

    88

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc -s -masm=intel $CFILE
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that prints a line to the standard error
+ * return 1 (success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+return (1);
+}
+
