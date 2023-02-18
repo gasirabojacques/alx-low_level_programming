b09eaa491d37667548b90ca97609533d1634e577
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-02-18 07:09:43 -0800

    my quinno

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,13 +6,13 @@
  * Return: always 0
  */
 
-int main (void)
+int main(void)
 {
-	char i;
-	for (i = 'a' ; i <= 'z' ; i++)
-	      putchar(i);
-	for (i = 'A' ; i <= 'Z' ; i++)
-	      putchar(i);
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+putchar(i);
+for (i = 'A' ; i <= 'Z' ; i++)
+putchar(i);
 putchar('\n');
-	 return (0);
+return (0);
 }
