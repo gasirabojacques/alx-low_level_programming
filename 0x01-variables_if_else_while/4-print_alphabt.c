6a1d650c0978425a936367ddc014a9f5847053f9
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-02-18 07:13:51 -0800

    agatwiko

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	char i;
-	for (i = 'a' ; i <= 'z' ; i++)
-		if (i != 'q' && i != 'e')
-			putchar(i);
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+if (i != 'q' && i != 'e')
+putchar(i);
 putchar('\n');
-	return (0);
+return (0);
 }
