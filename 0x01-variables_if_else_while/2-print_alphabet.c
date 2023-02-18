63a98a9c4c61aefad8db4eccd15627a7fc8d57ad
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-02-17 18:28:53 -0800

    number 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char c;
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++; }
+	 purchar ("\n");
+	return (0);
+}
