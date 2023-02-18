3daf5cf155dffd0a475f9f8ea937a4d02b0ba0d4
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-02-18 06:53:23 -0800

    bad day

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - main function
@@ -8,10 +8,11 @@
 
 int main(void)
 {
-for (int i = 0; i < 10 ; i++)
+int i;
+for (i = 0; i < 10 ; i++)
 {
 putchar(i + '0');
 }
-putchar("\n");
+putchar('\n');
 return (0);
 }
