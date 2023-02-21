7810bcebdecc5c9c14392692d7492eb57225c2ef
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-02-21 11:19:47 -0800

    my 1 commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet | function that prints alphabet in lowe case
+ *
+ * Return: always 0;
+ */
+
+
+void print_alphabet(void)
+{
+char j;
+
+for (j = '0' ; j <= 'j' ; j++)
+_putchar(j);
+_putchar('\n');
+}
