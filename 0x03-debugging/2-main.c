d471d11a521b92d9d8a28e9d65ef971adab10d9e
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-02-23 12:43:18 -0800

    my 3 commit

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
 * main - prints the largest of 3 integers
@@ -8,16 +8,8 @@
 
 int main(void)
 {
-int a, b, c;
-int largest ;
 
-a = 972;
-b = -98;
-c = 0;
-
-largest = largest  (a, b, c);
-
-printf("%d is the largest number\n", largest);
+printf("%d is the largest number\n", largest_number(972 , -98 , 0));
 
 return (0);
 }
