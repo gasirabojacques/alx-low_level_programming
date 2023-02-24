ed6298589bfc471331d43b1fb8513827b03f152a
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-02-24 15:55:19 -0800

    my 12 commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,16 +10,20 @@ int _putchar(char c);
 
 void print_number(int n)
 {
-unsigned int k = n;
-
+int divisor = 1;
 if (n < 0)
 {
+_putchar('-');
 n *= -1;
-k = n;
-_putchar('_');
 }
-k /= 10;
-if (k != 0)
-print_number(k);
-_putchar((unsigned int) n % 10 + '0');
+while (n / divisor >= 10)
+{
+divisor *= 10;
+}
+while (divisor != 0)
+{
+_putchar((n / divisor)+'0');
+n %= divisor;
+divisor /= 10;
+}
 }
