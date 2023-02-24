084f4b9b03ed50a6ffa7a831e0ced5fc422b2787
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-02-23 17:56:25 -0800

    my 3 commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#define _CRT_SECURE_NO_WORNINGS
+
+/**
+ * print_number - print the number since 0 up to 9
+ * Return: The numbers since 0 up to 9
+ */
+
+void print_numbers(void);
+for (x = 0; x <= 9 ; x++)
+{
+_putchar(x + '0')
+}
+_putchar('\n');
+}
+}
