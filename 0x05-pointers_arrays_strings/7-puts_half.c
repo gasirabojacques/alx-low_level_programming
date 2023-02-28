94230149b922d3e111a468b71674ab8b1d059195
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-02-27 16:38:45 -0800

    my 7 commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int _putchar(char c);
+/**
+ * puts_half - prints half of a string
+ * @str: The string to print
+ * Return: void
+ */
+
+void puts_half(char *str)
+
+{
+int j = 0;
+int g;
+
+while (str[j] != '\0')
+{
+j++;
+}
+if (j % 2 == 1)
+{
+g =((j - 1) / 2);
+g += 1;
+}
+else
+{
+g = j / 2;
+}
+
+
+for (; g < j ; g++)
+{
+_putchar(str[g]);
+}
+_putchar('\n');
+}
