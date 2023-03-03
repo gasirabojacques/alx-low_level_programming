93a38ba5dd92d89a26b5eac13c0ad5be348ff498
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-03-02 18:05:51 -0800

    final

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -4,22 +4,14 @@
 * You are not allowed to modify the variable p
 * You can only write one statement
 * You are not allowed to use ,
-* You are not allowed to code anything else than the line of expected line of code at the expected line
 * Your code should be written at line 19, before the ;
 * Do not remove anything from the initial code (not even the comments)
-* and don’t change anything but the line of code you are adding (don’t change the spaces to tabs!)
 * You are allowed to use the standard library
- */
-
+*/
 int main(void)
 {
-int n;
-int a[5];
-int *p;
-a[2] = 1024;
-p = &n;
-*(p + 5) = 98;
-/* so that this print 98\n*/
-printf("a[2] = %d\n", a[2]);
+int a[] = { 1, 2, 98, 4, 5 };
+int *p = a;
+printf("a[2] = %d\n", *(p + 2));
 return (0);
 }
