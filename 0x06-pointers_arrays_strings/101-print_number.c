6360a0733cf39e1daadf1705da62af6c780ed125
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-03-02 17:13:24 -0800

    my 9 commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,17 +2,14 @@
 int _putchar(char c);
 
 /**
- * print_number - print numbers chars
+ * print_number - print number chars
  * @n: integer params
- * Return: 0
+ * return: 0
  */
-
 void print_number(int n)
 {
 unsigned int n1;
-
 n1 = n;
-
 if (n < 0)
 {
 _putchar('-');
