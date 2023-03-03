a0da3ade2c61bbbc2501bfbbad845a6fadf8be8f
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-03-02 17:49:16 -0800

    slow

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -5,11 +5,13 @@ int n;
 int a[5];
 int *p;
 a[2] = 1024;
-p = 8n;
+p = &n;
 /*
  * write your line of code here
  * you are not allowed to use a
  * you are not allowed to modify p
+ * only one statement
+ * you can only not allowed to code anything else than this line of code
  */
 *(p + 5) = 98;
 /* so that this print 98\n*/
