3563cf8a1b89384f88b0892ce943a520fdfcb1e8
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-03-06 14:08:59 -0800

    my 6 commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+int _putchar(char c);
+
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int i, n;
+
+for (i = 0; i < 8; i++)
+{
+for (n = 0; n < 0; n++)
+_putchar(a[i][n]);
+_putchar('\n');
+}
+}
