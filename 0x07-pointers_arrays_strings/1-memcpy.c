a2cb5abe200c635315bb67ce882e5fd276ceb0d3
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-03-06 14:53:04 -0800

    my 1 commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of money to print
- * @size: the size of the memory to print
- *
- * Return: Nothing
+ * _memcpy - Entry point
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: Always 0
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-char *p = dest;
-while (n--)
-{
-*p++ = *src++;
-}
+unsigned int i;
+
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+
 return (dest);
-}
-int main(void)
-{
-char src[] = "Hello, world!";
-char dest[20];
-_memcpy(dest, src, sizeof(src));
-printf("%s\n", dest);
-return (0);
 }
