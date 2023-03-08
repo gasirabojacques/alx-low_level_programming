058f8674c4d2d389937ee32c64f6d5caf6d2fa91
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-03-08 15:58:58 -0800

    my 3 commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factor - calculate the factorial of a number
+ * @n: the number to calculate the factorial
+ *
+ * Return: integr value
+ */
+
+int factorial(int n)
+{
+if (n < 0)
+return (-1);
+
+if (n <= 1)
+return (1);
+
+return (n * factorial(n - 1));
+}
