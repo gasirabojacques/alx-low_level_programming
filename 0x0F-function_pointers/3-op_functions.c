a994534b42d8fa0442290c7b88f32e3fb05daa0f
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-03-22 18:39:53 -0700

    my 3 commit

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,13 +1,20 @@
 #include "3-calc.h"
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * op_add - Returns the sum of two numbers
+ * @a: The first number
+ * @b: The second number
+ * Return: The sum of a and b
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - Returns the sum of two numbers
  * @a: The first number
  * @b: The second number
  * Return: The sum of a and b
@@ -36,6 +43,11 @@ return (a * b);
  */
 int op_div(int a, int b)
 {
+if (b == 0)
+{
+printf("error\n");
+exit(100);
+}
 return (a / b);
 }
 
@@ -48,5 +60,10 @@ return (a / b);
 
 int op_mod(int a, int b)
 {
+if (b == 0)
+{
+printf("error\n");
+exit(100);
+}
 return (a % b);
 }
