4095e421743e335eb386577a3877633ceadd5667
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-04-19 14:39:00 -0700

    my 0 commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,35 +1,43 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stddef.h>
+
 /**
- * read_textfile - Reads a text file and prints it to standard output.
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
  * @filename: A pointer to the name of the file.
- * @letters: The number of letters the function should read and print.
+ * @letters: The number of letters the
+ *           function should read and print.
  *
  * Return: If the function fails or filename is NULL - 0.
  *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-FILE *file = fopen(filename, "r");
-char *buffer = (char *) malloc(letters + 1);
-size_t bytesRead;
+ssize_t o, r, w;
+char *buffer;
+
 if (filename == NULL)
-{
 return (0);
-}
-if (file == NULL)
-{
-return (0);
-}
+
+buffer = malloc(sizeof(char) * letters);
 if (buffer == NULL)
+return (0);
+
+o = open(filename, O_RDONLY);
+r = read(o, buffer, letters);
+w = write(STDOUT_FILENO, buffer, r);
+
+if (o == -1 || r == -1 || w == -1 || w != r)
 {
-fclose(file);
+free(buffer);
 return (0);
 }
-bytesRead = fread(buffer, 1, letters, file);
-buffer[bytesRead] = '\0';
-fwrite(buffer, 1, bytesRead, stdout);
-fclose(file);
+
 free(buffer);
-return (bytesRead);
+close(o);
+
+return (w);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,10 @@
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-size_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 size_t _strlen(char *str);
 int create_file(const char *filename, char *text_content);
 size_t _strlen(char *str);
