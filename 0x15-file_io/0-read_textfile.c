b21fd82a93a37d95b179865315fd9a67dc89395d
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-04-19 15:20:50 -0700

    my 0 commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,38 +7,36 @@
 
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
  *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t o, r, w;
-char *buffer;
+int dir;
+ssize_t d, r;
+char *buf;
 
-if (filename == NULL)
+if (!filename)
 return (0);
 
-buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
+dir = open(filename, O_RDONLY);
+
+if (dir == -1)
 return (0);
 
-o = open(filename, O_RDONLY);
-r = read(o, buffer, letters);
-w = write(STDOUT_FILENO, buffer, r);
-
-if (o == -1 || r == -1 || w == -1 || w != r)
-{
-free(buffer);
+buf = malloc(sizeof(char) * (letters));
+if (!buf)
 return (0);
-}
 
-free(buffer);
-close(o);
+d = read(dir, buf, letters);
+r = write(STDOUT_FILENO, buf, d);
 
-return (w);
+close(dir);
+
+free(buf);
+
+return (r);
 }
