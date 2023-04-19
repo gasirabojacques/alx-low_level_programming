75bc79ba3340fc7123209c068287318c627712dd
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-04-19 14:15:20 -0700

    my 1 commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,8 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
  * create_file - Creates a file.
@@ -10,24 +14,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-int o, w, len = 0;
+int k, z, length = 0;
 
 if (filename == NULL)
 return (-1);
 
 if (text_content != NULL)
 {
-for (len = 0; text_content[len];)
-len++;
+for (length = 0; text_content[length];)
+length++;
 }
 
-o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-w = write(o, text_content, len);
+k = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+z = write(k, text_content, length);
 
-if (o == -1 || w == -1)
+if (k == -1 || z == -1)
 return (-1);
 
-close(o);
+close(k);
 
 return (1);
 }
