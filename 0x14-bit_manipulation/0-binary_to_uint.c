6f8b424dafadd0d6d81dc6d6b14bebb9a8cd6640
gasirabojacques <jacquesishimwegasirabo@gmail.com>
2023-05-04 11:04:39 -0700

    a kind of sheet

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: pointer to a str containing a binary number
- * Return: the converted number, or 0 if b containers a char not 0 or 1
- * or b is NULL
+ * binary_to_uint - convert a binary number to an unsigned int
+ * @b: char string
+ * Return: converted decimal number or 0 if there is an unconvertable char
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int num = 0;
-int i;
+unsigned int total, power;
+int len;
 
-if (!b)
+if (b == NULL)
 return (0);
 
-for (i = 0; b[i] != '\0'; i++)
+for (len = 0; b[len]; len++)
 {
-if (b[i] != '0' && b[i] != '1')
+if (b[len] != '0' && b[len] != '1')
 return (0);
-
 }
-for (i = 0; b[i] != '\0'; i++)
+
+for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
 {
-num <<= 1;
-if (b[i] == '1')
-num += 1;
+if (b[len] == '1')
+total += power;
 }
-return (num);
 
+return (total);
 }
